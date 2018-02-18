9cf04b9812156f7a6eb7b458702dc0fdcaf88428
Santiados <santireven20@gmail.com>
2018-02-19 00:57:22 +0100

    pri

---
+++ js/info.php
@@ -0,0 +1,31 @@
+<?php 
+
+// $info = [
+// 	'X' => [
+// 		'nombre' => 'Modelo X',
+// 		'ram' => '3GB'
+// 	],
+// 	'Y' => [
+// 		'nombre' => 'Modelo Y',
+// 		'ram' => '10GB'
+// 	]
+// ];
+
+// $peticion = strtolower($_REQUEST['nombre']);
+// $resultado = [];
+// if($peticion!==''){
+// 	foreach($info as $clave => $valor){
+// 			if ($clave == $peticion) {
+// 					$resultado[] = $clave;
+// 			}
+// 	}
+// }
+
+// if(count($resultado)==0){
+	echo 'No hay nada relacionado';
+// }else {
+	// return $resultado; 
+// }
+
+
+?>
